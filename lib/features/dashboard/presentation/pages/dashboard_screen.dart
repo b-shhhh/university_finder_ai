465b8afc47020990355a27c32c4adb496f07bb45
b-shhhh <159143153+b-shhhh@users.noreply.github.com>
2026-02-25 20:12:18 +0545

    sensor

--- lib/features/dashboard/presentation/pages/dashboard_screen.dart
+++ lib/features/dashboard/presentation/pages/dashboard_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 import '../../../../core/api/api_client.dart';
 import '../../../../core/api/api_endpoints.dart';
