51f5d5a3f0201c59fed45e580a734c2d1980427f
b-shhhh <159143153+b-shhhh@users.noreply.github.com>
2026-03-04 18:36:47 +0545

    not needed

--- backend/src/routes/recommendation.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getRecommendations } from "../controllers/recommendation.controller";
-
-const router = Router();
-
-router.get("/", getRecommendations);
-
-export default router;
